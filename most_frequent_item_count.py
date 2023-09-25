853ebf1febf5488913981cab3a8e4af5177ea555
betamemo <bandita.kul@gmail.com>
2023-09-25 14:09:32 +0700

    import Counter

--- most_frequent_item_count.py
+++ most_frequent_item_count.py
@@ -1,16 +1,10 @@
+from collections import Counter
+
 def most_frequent_item_count(collection):
     # Do your magic. :)
     
-    item_dict = {}
-    freq_item = 0
-    
-    for c in collection:
-        if c in item_dict:
-            item_dict[c] = item_dict[c] + 1
-        else:
-            item_dict[c] = 1
-    
-    for d in item_dict:
-        if item_dict[d] > freq_item:
-            freq_item = item_dict[d]
-    return freq_item
+    if not collection:
+        return 0
+    else:
+        counter = Counter(collection)
+        return counter.most_common(1)[0][1]
